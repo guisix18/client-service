c4cdcafcb0ba405fff45cf72d39a0c914923caee
Guilherme <guilhermeshadow91@gmail.com>
2024-04-14 19:09:24 -0300

    feat: swagger first config

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,29 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('Client Services')
+    .setDescription('API to access client services')
+    .addBearerAuth(
+      {
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'Bearer ',
+      },
+      'access_token',
+    )
+    .build();
+
+  const doc = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('doc/client', app, doc);
+
+  app.useGlobalPipes(new ValidationPipe());
+
+  await app.listen(3001);
 }
 bootstrap();
