4e636cf5a35d789a9af8f17e2a5b6bc86b6e4124
Guilherme <guilhermeshadow91@gmail.com>
2024-04-19 19:19:04 -0300

    feat: microservice conexion

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,22 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.KAFKA,
+    options: {
+      client: {
+        brokers: ['localhost:9092'],
+      },
+      consumer: {
+        groupId: 'transaction-consumer',
+      },
+    },
+  });
 
   const config = new DocumentBuilder()
     .setTitle('Client Services')
@@ -19,11 +32,12 @@ async function bootstrap() {
     )
     .build();
 
+  app.useGlobalPipes(new ValidationPipe());
+
   const doc = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('doc/client', app, doc);
 
-  app.useGlobalPipes(new ValidationPipe());
-
-  await app.listen(3001);
+  await app.startAllMicroservices();
+  await app.listen(3000);
 }
 bootstrap();
